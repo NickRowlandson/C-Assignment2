954dc44ec7ba9a37fe89f904fc96993e28f26639
NickRowlandson <nicholasrowlandson@gmail.com>
2016-08-03 12:37:31 -0400

    Setup basic structure for project

--- C++Assignment2/Header.h
+++ C++Assignment2/Header.h
@@ -1,6 +1,33 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+// HighScoreManager class
+class HighScoreManager {
+public:
+	HighScoreManager();
+	void CreateHighScore();
+	void UpdateHighScore();
+	void PrintHighScore();
 
+private:
+	string username;
+	int score;
+	string date;
+};
+
+// User class
+class User {
+public:
+	User();
+	void Login();
+	void CheckIfUserExist();
+	void UpdateUserList();
+	void DeleteUser();
+	void SetHighScore();
+
+private:
+	string username;
+	string firstNameLastName;
+};
 
 #endif
--- C++Assignment2/Source.cpp
+++ C++Assignment2/Source.cpp
@@ -8,6 +8,47 @@
 #include <regex>
 #include "Header.h"
 
+using namespace std;
 
 
+// HIGHSCOREMANAGER CLASS METHODS
+void HighScoreManager::CreateHighScore() {
+
+}
+
+void HighScoreManager::UpdateHighScore() {
+
+}
+
+
+void HighScoreManager::PrintHighScore() {
+
+}
+
+// USER CLASS METHODS
+void User::Login() {
+
+}
+
+void User::CheckIfUserExist() {
+
+}
+
+void User::UpdateUserList() {
+
+}
+
+void User::DeleteUser() {
+
+}
+
+void User::SetHighScore() {
+
+}
+
+
+// MAIN
+int main(){
+
+};
 
